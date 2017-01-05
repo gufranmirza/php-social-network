ccb88de057634eb6d7f03c83c08b32808ec59b5b
gufranmirza <gufranmirza1@gmail.com>
2017-01-05 20:06:59 +0530

    Update connect.inc.php

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,30 +1,3 @@
 <?php
-
     $conn = mysqli_connect("localhost", "root","", "findfriends") or die("Cant connect");
-	
-
-
-/*
-
-//store the URL into a variable
-$url = 'http://www.youtube.com/watch?v=x6qe_kVaBpg';
- 
-//extract the ID
-preg_match(
-        '/[\?\&]v=([^\?\&]+)/',
-        $url,
-        $matches
-    );
- 
-//the ID of the YouTube URL: x6qe_kVaBpg
-$id = $matches[1];
- 
-//set a custom width and height
-$width = '640';
-$height = '360';
- 
-//echo the embed code. You can even wrap it in a class
-echo '<div class="youtube-article"><iframe class="dt-youtube" width="' .$width. '" height="'.$height.'" src="//www.youtube.com/embed/'.$id.'" frameborder="0" allowfullscreen></iframe></div>';
-*/
-
 ?>
