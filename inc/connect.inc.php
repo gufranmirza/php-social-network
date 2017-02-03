fc4a338b67ae53e1583266b98197aa2962d113d3
gufranmirza <gufranmirza1@gmail.com>
2017-02-03 20:30:37 +0530

    Update connect.inc.php

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,3 +1,8 @@
 <?php
     $conn = mysqli_connect("localhost", "root","", "findfriends") or die("Cant connect");
+    //change the db connection as follows
+    //replace 'localhost' with you host if it is on local machine it would be 'localhost'
+    //update username 'root' with your database username
+    //update the password " " with your password for your database user
+    //import the database to your mysql from the folder "db" a new database will be created named as "findfriends" by default
 ?>
