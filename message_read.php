1dee105eb901be40a047167b2fb0aab9cba812d8
gufran mirza <gufranmirza1@gmail.com>
2017-02-17 02:08:19 +0530

    Update message_read.php

--- message_read.php
+++ message_read.php
@@ -1,17 +1,12 @@
-
-
 <?php
-    
 	include("./inc/header.inc.php");
-    // get message for logged in user
+	// get message for logged in user
 	echo '<h2> My Unread messages</h2><br/>';
-
-    $query = "SELECT * FROM `pvt_messages` WHERE `user_to` = '$user' && `opened` = 'no' && `deleted` = 'no' ";
+	$query = "SELECT * FROM `pvt_messages` WHERE `user_to` = '$user' && `opened` = 'no' && `deleted` = 'no' ";
 	$grab_messages = mysqli_query($conn, $query);
 	$rows = mysqli_num_rows($grab_messages);
 	if($rows != 0){
 	while($get_msg = mysqli_fetch_assoc($grab_messages)){
-		
 		$id = $get_msg['id'];
 		$from = $get_msg['user_from'];
 		$to = $get_msg['user_to'];
@@ -20,8 +15,6 @@
 		$opened = $get_msg['opened'];
 		$deleted = $get_msg['deleted'];
 ?>
-
-
 <script language="javascript">
          function toggle<?php echo $id; ?>() {
            var ele = document.getElementById("toggleText<?php echo $id; ?>");
@@ -38,73 +31,49 @@
 
 <?php		
 		if(strlen($msg_title) >50){
-			
 			$body = substr($msg_title, 0, 50).'.....';
 			echo '<br/>'.$msg_title.'<br/><br/><hr/>';
-			
 		}else
-		
-		
 		if(strlen($body) >150){
-			
 			$body = substr($body, 0, 150).'.....';
 			echo '<br/>'.$body.'<br/><br/><hr/>';
-			
-		}else{
-			
-		$body = $body;	
-			
+		}else{		
+		$body = $body;				
 		echo '		
 		<form action="message_read.php" method="POST"  name="msg_title" id="button_msg">
-		<br/><a href='.$from.'>'.$from.'</a>
-		<input type="button" name="openmsg" value="'.$msg_title.'" onclick="javascript:toggle'.$id.'()"/>
-		<input type="submit" name="setopened_'.$id.'" value="Set As Read" >
-		
-		
+			<br/><a href='.$from.'>'.$from.'</a>
+			<input type="button" name="openmsg" value="'.$msg_title.'" onclick="javascript:toggle'.$id.'()"/>
+			<input type="submit" name="setopened_'.$id.'" value="Set As Read" >
 		</form>
-		
-		
-		
-		
 		<div id="toggleText'.$id.'" style="display:none" >'.$body.'</div>';	
-		
-		if(isset($_POST['setopened_'.$id])){
 			
+		if(isset($_POST['setopened_'.$id])){
 			$query = "UPDATE `pvt_messages` SET `opened`= 'yes' WHERE `msg_title` = '$msg_title' && `user_to` = '$user' ";
 			$run = mysqli_query($conn, $query);
 			header("Location:message_read.php");
-			
 		}
-
-		
-	
-			
 		}
-		
-		
-		
-	}	
-	}else{
-		echo 'You have not any unread messages yet<br>';
-	}
-	
-	
-	echo '<br/><h2> My Read messages</h2><br/>';
+		}	
+		}
+		else{
+			echo 'You have not any unread messages yet<br>';
+		}
+		echo '<br/><h2> My Read messages</h2><br/>';
 
-    $query = "SELECT * FROM `pvt_messages` WHERE `user_to` = '$user' && `opened` = 'yes' && `deleted` = 'no'";
-	$grab_messages = mysqli_query($conn, $query);
-	$rows = mysqli_num_rows($grab_messages);
-	
-	if($rows != 0){
-	while($get_msg = mysqli_fetch_assoc($grab_messages)){
-		
-		$id = $get_msg['id'];
-		$from = $get_msg['user_from'];
-		$to = $get_msg['user_to'];
-		$msg_title = $get_msg['msg_title'];
-		$body = $get_msg['msg_body'];
-		$opened = $get_msg['opened'];
-		$deleted = $get_msg['deleted'];
+	    $query = "SELECT * FROM `pvt_messages` WHERE `user_to` = '$user' && `opened` = 'yes' && `deleted` = 'no'";
+		$grab_messages = mysqli_query($conn, $query);
+		$rows = mysqli_num_rows($grab_messages);
+
+		if($rows != 0){
+		while($get_msg = mysqli_fetch_assoc($grab_messages)){
+
+			$id = $get_msg['id'];
+			$from = $get_msg['user_from'];
+			$to = $get_msg['user_to'];
+			$msg_title = $get_msg['msg_title'];
+			$body = $get_msg['msg_body'];
+			$opened = $get_msg['opened'];
+			$deleted = $get_msg['deleted'];
 ?>
 
 <script language="javascript">
@@ -162,17 +131,4 @@
 		echo 'You have not read any messages yet';
 		
 	}
-	
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
